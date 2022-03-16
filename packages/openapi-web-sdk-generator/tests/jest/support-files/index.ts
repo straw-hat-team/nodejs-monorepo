822223f70df2205a32306f1776c57efe67a22770
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-03-15 22:48:46 -0400

    fix tmp dir location

--- packages/openapi-web-sdk-generator/tests/jest/support-files/index.ts
+++ packages/openapi-web-sdk-generator/tests/jest/support-files/index.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { readOpenApiFile } from '../../../src/helpers';
 import { Dir } from '../../../src/dir';
 
-const TMP_BASE_DIR = path.resolve(__dirname, '..', '..', '..', '.tmp');
+const TMP_BASE_DIR = path.resolve(__dirname, '..', '..', '..', 'tmp');
 const PET_STORE_FILE_PATH = path.resolve(__dirname, 'pet-store.json');
 
 export async function createTmpDir(dirPath: string[]) {
