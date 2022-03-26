d0766d1a68ac38cbc7c362071871482c7cd7623e
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-03-26 15:12:37 -0400

    chore: replace ts-jest with @swc/jest

--- packages/@straw-hat/fetcher/jest.config.js
+++ packages/@straw-hat/fetcher/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/jest/jest.setup.ts'],
 };
