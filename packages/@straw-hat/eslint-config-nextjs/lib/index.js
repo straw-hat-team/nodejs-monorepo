cbd907af7ae8b987b596eda08e33380659bab2b6
Yordis Prieto <yordis.prieto@gmail.com>
2024-10-29 22:29:58 -0400

    fix: ts unused vars

--- packages/@straw-hat/eslint-config-nextjs/lib/index.js
+++ packages/@straw-hat/eslint-config-nextjs/lib/index.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ]
+  }
 };
